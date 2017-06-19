7661c3456981ca3025546030e018f601958e36d9
fafuuu <fafuuu96@gmail.com>
2017-06-19 11:26:13 +0200

    user route

--- Dienstgeber/app.js
+++ Dienstgeber/app.js
@@ -1,1 +1,10 @@
+var express = require('express');
 
+var app = express();
+
+app.get('/user', function(req, res) {
+	res.status(200);
+	res.send("Listet alle Benutzer auf");
+});
+
+app.listen(3000);
