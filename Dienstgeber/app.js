6dfdc55c8b390ff3b93a57bb847014bf6643e071
fafuuu <fafuuu96@gmail.com>
2017-06-19 11:58:47 +0200

    Anfang REST Implementierung

--- Dienstgeber/app.js
+++ Dienstgeber/app.js
@@ -7,4 +7,29 @@ app.get('/user', function(req, res) {
 	res.send("Listet alle Benutzer auf");
 });
 
+app.get('/user/:id', function(req, res) {
+	var id = req.params.id;
+	res.status(200);
+	res.send("User mit der id " + id);
+});
+
+app.post('/user/new', function(req, res) {
+	console.log("User hinzugefügt");
+});
+
+
+app.get('/user/:id/book', function(req, res) {
+
+	var id = req.params.id;
+	res.status(200);
+	res.send("Alle Bücher von Nutzer " + id);
+});
+
+app.post('/user/:id/book', function(req, res) {
+
+	var id = req.params.id;
+	res.status(200);
+	console.log("Neues Buch anlegen");
+});
+
 app.listen(3000);
